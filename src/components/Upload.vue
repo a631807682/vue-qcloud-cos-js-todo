98bf766f016825e4f9fa5ee4adfc8187811bfc24
陈睿 <chenrui1@ivvi.com>
2018-01-17 13:43:34 +0800

    tips

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -3,7 +3,7 @@
 	<ul>
 		<li>修改lib/config中bucketName</li>
 		<li>腾讯cos后台添加cors</li>
-		<li>前端授权测试阶段修改lib/qcloud文件SecretId与MjwzXXXXXXXXXX值</li>
+		<li>前端授权测试阶段修改lib/qcloud文件SecretId与SecretKey值</li>
 	</ul>
 		<el-upload action="" :http-request="handleCosUpload" list-type="picture-card" :on-success="handleUploadSuccess" accept="image/*">
 			<i class="el-icon-plus"></i>
